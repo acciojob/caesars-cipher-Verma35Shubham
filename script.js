7d258ee5c7a648c3717ef2ff687e8ae20137eee3
Shubham Verma <verma.shubham2537@gmail.com>
2023-05-14 17:24:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Your Script here.
+/// Your Script here.
 
 const lookup = {
   'A': 'N','B': 'O','C': 'P','D': 'Q',
@@ -13,24 +13,19 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
-	for (let i = 0; i < encodedStr.length; i++) {
-    const charCode = encodedStr.charCodeAt(i);
-    if (charCode >= 65 && charCode <= 90) { // A-Z
-      let newCharCode = charCode + 13;
-      if (newCharCode > 90) {
-        newCharCode -= 26;
-      }
-      decodedArr.push(lookup[String.fromCharCode(newCharCode)]);
-    } else {
-      decodedArr.push(encodedStr[i]);
-    }
-  }
+	for(let i = 0 ; i < encodedStr.length ; i++) {
+        if(lookup[ encodedStr[i] ] === undefined){
+            decoderArr.push( encodedStr[i] ) ;
+        }   
+        else{ 
+            decoderArr.push(lookup[ encodedStr[i] ]);
+        }
 
-  return decodedArr.join('');//return decodedArr
+  return decodedArr;//return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 module.exports = rot13;
